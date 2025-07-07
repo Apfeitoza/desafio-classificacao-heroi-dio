11ba659816b4ff9e679c215d0064e1376e7d276e
Apfeitoza <annapaulaasf@gmail.com>
2025-07-07 19:55:40 -0300

    Submit do desafio da Dio: Classificação do Nível de Herói

---
+++ index.js
@@ -0,0 +1,24 @@
+let nomeHeroi = "Knutzz"
+let xp = 12550
+let nivelHeroi = ""
+
+if (xp <= 1000){
+    nivelHeroi = "Ferro";
+}else if (xp >=1001 && xp <=2000){
+    nivelHeroi = "Bronze";
+}else if (xp >=2001 && xp <=5000){
+    nivelHeroi = "Prata";
+}else if (xp >=5001 && xp <=7000){
+    nivelHeroi = "Ouro";
+}else if (xp >=7001 && xp <=8000){
+    nivelHeroi = "Platina";
+}else if(xp >=8001 && xp <=9000){
+    nivelHeroi = "Ascendente";
+}else if (xp >= 9001 && xp <=10000){
+    nivelHeroi = "Imortal";
+}else{
+    nivelHeroi = "Radiante";
+}
+
+//Saída
+console.log(`O herói de nome ${nomeHeroi} está no nível de: ${nivelHeroi}!`)
